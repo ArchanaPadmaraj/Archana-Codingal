6623d1a8a97e36480184cbb3009d6f59a05e1838
ArchanaPadmaraj <archanapadmaraj.46162@gmail.com>
2025-06-18 22:24:45 +0530

    Lesson 19

---
+++ Lesson 19 - intro to javascript/ACP.js
@@ -0,0 +1,12 @@
+var name = "Archana Padmaraj"
+var age = 15
+var grade = "10th grade"
+var skills = "css and html"
+var hobby = "code"
+var siblings = "i have two siblings"
+var subject = "My favorite subject is Maths"
+var color = "My favorite color is green"
+var education ="I am studying in kottyam, kerala"
+var conclusion ="Thats all about me! thank you!"
+
+console.log(`Hi my name is ${name}\n I am ${age}, and I am in ${grade}.\n${siblings} ${education} My skills are ${skills} and in my free time i like to ${hobby} ${subject} ${color} ${conclusion} `)
---
+++ Lesson 19 - intro to javascript/a1.js
@@ -0,0 +1,32 @@
+var name = "archana"
+var age = 15
+var grade = "10th grade"
+var skills = "css and html"
+var hobby = "coding"
+
+console.log(`Hi my name is ${name}\n I am ${age}, and am in ${grade}. My skills are ${skills} and i in my free time i like to do ${hobby}`)
+
+var a = 10
+var b = 3
+console.log(a+b)
+console.log(a-b)
+console.log(a*b)
+console.log(a/b)
+console.log(a%b)
+
+var x = 5
+var y = "5"
+console.log(x==y)
+console.log(x===y)
+console.log(x!=y)
+console.log(x!==y)
+console.log(x<y)
+console.log(x<=y)
+console.log(x>y)
+console.log(x>=y)
+
+var p = true
+var q = false
+console.log(p&&q)
+console.log(p||q)
+console.log(!p)
