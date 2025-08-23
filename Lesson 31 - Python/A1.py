cf4cade4a49d276f364ff41a5ec7a0ab024a80e6
ArchanaPadmaraj <archanapadmaraj.46162@gmail.com>
2025-08-23 22:37:17 +0530

    Lesson 31 - Python

---
+++ Lesson 31 - Python/A1.py
@@ -0,0 +1,16 @@
+# beware indentations, also curly brackets dont work in scope work colon is used to define scope
+# activity 01
+name = "Archana"
+age = "15 year old"
+studying = "10th grade"
+DOB = "4th August 2010"
+color = "green"
+print(f"hi my name is {name} and i am a {age} ", end=",")
+print(f"born on {DOB} and currently in {studying}", end=".")
+print(f"my favorite color is {color}")
+
+# activity 02
+
+print("the keywords in Python are:")
+import keyword
+print(keyword.kwlist)
