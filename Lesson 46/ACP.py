2b5b209d065ecb19fe599ba0db25c1b0ef6a57f2
ArchanaPadmaraj <archanapadmaraj.46162@gmail.com>
2025-10-29 22:31:38 +0530

    edits made

--- Lesson 46/ACP.py
+++ Lesson 46/ACP.py
@@ -1,7 +1,7 @@
 def project (n):
     iteration = 0
-    for i in range(0,n+1):
-        for j in range(0,n+1):
+    for i in range(0,n):
+        for j in range(0,n):
             print("*",end=" ")
             iteration +=1
         print(" ")
